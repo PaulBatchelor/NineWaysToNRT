3bf38f3bbda81d00a18718cddfd798348f9f0fcf
Paul <foo>
2014-12-30 11:29:26 -0500

    XM changes

--- 05_XM/create.sh
+++ 05_XM/create.sh
@@ -1,2 +1,4 @@
-nrt_string="d4. r8 m s t4 s m r2 t4 (D2. m s)"
-echo $nrt_string | nrt -F' ' | sort -n | ./nrt2xm out.xm 120 70 
+nrt_string="d4. r8 m s | t4 s m | r2 t4 | (D2. m s)"
+BPM=120
+offset=70
+echo $nrt_string | nrt -F' ' | sort -n | ./nrt2xm out.xm $BPM $offset
